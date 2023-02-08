2e8f4a93423b1a271c645887413a0138020d9b69
simranms <simran@scaledge.io>
2023-02-08 10:49:33 +0530

    adder_unit

---
+++ add.v
@@ -0,0 +1,9 @@
+module addr(input [2:0]a1,
+            input [2:0]b1,
+            input c1_input,
+            output reg c1_output,
+            output reg [3:0]sum1);
+  always@(a1 or b1 or c1_input) begin
+    {c1_output, sum1}=a1|b1|c1_input;
+  end
+endmodule
