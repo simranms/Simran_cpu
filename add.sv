93b774cfd0c37574e6b18faab8dc27463dcbd025
simranms <123356413+simranms@users.noreply.github.com>
2023-02-08 11:03:33 +0530

    Update add.sv

--- add.sv
+++ add.sv
@@ -4,6 +4,6 @@ module addr(input [2:0]a1,
             output reg c1_output,
             output reg [3:0]sum1);
   always@(a1 or b1 or c1_input) begin
-    {c1_output, sum1}=a1|b1|c1_input;
+              {c1_output, sum1}=a1+b1+c1_input;
   end
 endmodule
