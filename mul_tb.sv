39857fd94044c427f0cc80e630a4aa6c08a45eb3
simranms <123356413+simranms@users.noreply.github.com>
2023-02-09 14:47:10 +0530

    mul_unit

---
+++ mul_tb.sv
@@ -0,0 +1,28 @@
+module top;
+  
+  bit [3:0] a, b;
+  bit [7:0] c; 
+  
+  MUL#(4)  M(
+    .a2(a),
+    .b2(b),
+    .sum2(c)
+  );
+  
+  initial begin
+    $monitor("%d * %d = %d",a,b,c);
+    #1;
+    a = $urandom_range;
+    b = $urandom_range;
+    #1
+   $monitor("[%0t] a=%0d b=%0d c=%0d out=%0h", $time,a,b,out);
+    #10;
+    $finish;
+  end
+  
+    
+  initial begin
+     $dumpfile("abc.vcd");
+    $dumpvars(0);
+  end
+endmodule
